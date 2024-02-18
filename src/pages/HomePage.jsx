aeeab5c604638afcc32c275ce8b11ca5b74b4eb6
Tran Van Khoa <khoatranvan245@gmail.com>
2024-02-18 11:19:24 +0700

    add logo animation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar/NavBar'
 
 const HomePage = () => {
-  return <h1>Home Page</h1>
+  return <h1>Home page</h1>
 }
 
 export default HomePage
